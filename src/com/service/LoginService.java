ba833d478cf11f63fba4dc34db79eccc8beed4ce
pranavkneeraj <pranav.k@amazatic.com>
2016-04-24 11:56:52 +0530

    method changed as get for login

--- src/com/service/LoginService.java
+++ src/com/service/LoginService.java
@@ -2,6 +2,7 @@ package com.service;
 
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -10,7 +11,7 @@ import javax.ws.rs.core.MediaType;
 @Path("/login")
 public class LoginService {
 
-	@POST
+	@GET
 	@Produces(MediaType.APPLICATION_XML)
 	public String registerMobileNo(@HeaderParam("mobileNo") String mobileNo){
 		
